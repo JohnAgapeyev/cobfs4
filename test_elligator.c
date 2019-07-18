78c8bc720422943a26b2607a389c357edf9fc311
John Agapeyev <jagapeyev@gmail.com>
2019-07-17 21:20:59 -0700

    Reworked elligator test to run 10k times without interaction

--- test_elligator.c
+++ test_elligator.c
@@ -10,78 +10,86 @@
 void test_elligator(void) {
     EVP_PKEY *pkey;
     EVP_PKEY *peerkey;
+    EVP_PKEY_CTX *pctx;
     size_t skeylen;
     unsigned char *skey;
     unsigned char *skey2;
     unsigned char *skey3;
+    int count;
+    int good;
+    int bad;
+    int invalid;
 
-    EVP_PKEY_CTX *pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_X25519, NULL);
+    /* size_t i; */
 
-    BIGNUM *p;
-    BIGNUM *tmp;
-    BIGNUM *x;
-    BN_CTX *bnctx;
-    size_t i;
+    count = 0;
 
-    p = BN_new();
-    tmp = BN_new();
-    x = BN_new();
-    bnctx = BN_CTX_new();
+    good = 0;
+    bad = 0;
+    invalid = 0;
 
+    pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_X25519, NULL);
     pkey = EVP_PKEY_new();
 
     EVP_PKEY_keygen_init(pctx);
-    EVP_PKEY_keygen(pctx, &pkey);
 
-    EVP_PKEY_get_raw_public_key(pkey, NULL, &skeylen);
+    for (count = 0; count < 10000; ++count) {
+        EVP_PKEY_keygen(pctx, &pkey);
 
-    skey = OPENSSL_malloc(skeylen);
-    skey2 = OPENSSL_malloc(skeylen);
+        EVP_PKEY_get_raw_public_key(pkey, NULL, &skeylen);
 
-    if (!EVP_PKEY_get_raw_public_key(pkey, skey, &skeylen)) {
-        printf("Get raw call failed\n");
-        printf("%s\n", ERR_error_string(ERR_get_error(), NULL));
-    }
+        skey = OPENSSL_malloc(skeylen);
+        skey2 = OPENSSL_malloc(skeylen);
 
-    skey3 = elligator2(pkey);
-
-    if (skey3) {
-        peerkey = elligator2_inv(skey3);
-        if (peerkey) {
-            if (!EVP_PKEY_get_raw_public_key(peerkey, skey2, &skeylen)) {
-                printf("Get raw call failed\n");
-                printf("%s\n", ERR_error_string(ERR_get_error(), NULL));
-            }
-            for (i = 0; i < 32; ++i) {
-                printf("%02x", skey[i]);
-            }
-            printf("\n");
-            for (i = 0; i < 32; ++i) {
-                printf("%02x", skey2[i]);
-            }
-            printf("\n");
-            if (memcmp(skey, skey2, 32) == 0) {
-                printf("Elligator works as intended\n");
-            } else {
-                printf("Elligator FAILED\n");
-            }
-            EVP_PKEY_free(peerkey);
+        if (!EVP_PKEY_get_raw_public_key(pkey, skey, &skeylen)) {
+            /*
+            printf("Get raw call failed\n");
+            printf("%s\n", ERR_error_string(ERR_get_error(), NULL));
+            */
         }
-    } else {
-        printf("Generated key was not valid for elligator2\n");
+
+        skey3 = elligator2(pkey);
+
+        if (skey3) {
+            peerkey = elligator2_inv(skey3);
+            if (peerkey) {
+                if (!EVP_PKEY_get_raw_public_key(peerkey, skey2, &skeylen)) {
+                    /*
+                    printf("Get raw call failed\n");
+                    printf("%s\n", ERR_error_string(ERR_get_error(), NULL));
+                    */
+                }
+                /*
+                for (i = 0; i < 32; ++i) {
+                    printf("%02x", skey[i]);
+                }
+                printf("\n");
+                for (i = 0; i < 32; ++i) {
+                    printf("%02x", skey2[i]);
+                }
+                printf("\n");
+                */
+                if (memcmp(skey, skey2, 32) == 0) {
+                    /* printf("Elligator works as intended\n"); */
+                    ++good;
+                } else {
+                    /* printf("Elligator FAILED\n"); */
+                    ++bad;
+                }
+                EVP_PKEY_free(peerkey);
+            }
+        } else {
+            /* printf("Generated key was not valid for elligator2\n"); */
+            ++invalid;
+        }
+
+        OPENSSL_free(skey);
+        OPENSSL_free(skey2);
+        OPENSSL_free(skey3);
     }
-
-    OPENSSL_free(skey);
-    OPENSSL_free(skey2);
-    OPENSSL_free(skey3);
-
-    BN_free(p);
-    BN_free(tmp);
-    BN_free(x);
-
-    BN_CTX_free(bnctx);
-
     EVP_PKEY_free(pkey);
 
     EVP_PKEY_CTX_free(pctx);
+
+    printf("Elligator test ran %d times\nResults:\nGood: %d\nBad: %d\nInvalid: %d\n", count, good, bad, invalid);
 }
