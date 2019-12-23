39e67bf3e167db290d28d912a69bbbd2b0d245c2
John Agapeyev <jagapeyev@gmail.com>
2019-12-22 22:25:26 -0800

    Started rough draft of stream/global API

--- src/stream.c
+++ src/stream.c
@@ -1,8 +1,57 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <openssl/evp.h>
+
 #include "constants.h"
 #include "stream.h"
 
+static int perform_client_handshake(struct cobfs4_stream *stream) {
+    return 0;
+}
+
+static int perform_server_handshake(struct cobfs4_stream *stream) {
+    return 0;
+}
+
+int cobfs4_client_init(struct cobfs4_stream *stream, int socket) {
+    memset(stream, 0, sizeof(*stream));
+    stream->fd = socket;
+    stream->type = COBFS4_CLIENT;
+
+    if (perform_client_handshake(stream)) {
+        return -1;
+    }
+
+    stream->initialized = true;
+    return 0;
+}
+
+int cobfs4_server_init(struct cobfs4_stream *stream, int socket) {
+    memset(stream, 0, sizeof(*stream));
+    stream->fd = socket;
+    stream->type = COBFS4_SERVER;
+
+    if (perform_server_handshake(stream)) {
+        return -1;
+    }
+
+    stream->initialized = true;
+    return 0;
+}
+
+int cobfs4_read(struct cobfs4_stream * restrict stream, uint8_t buffer[static restrict COBFS4_MAX_DATA_LEN]) {
+    if (!stream->initialized) {
+        return -1;
+    }
+    return 0;
+}
+
+int cobfs4_write(struct cobfs4_stream *restrict stream, uint8_t * restrict buffer, size_t buf_len) {
+    if (!stream->initialized) {
+        return -1;
+    }
+    return 0;
+}
 
--- src/stream.h
+++ src/stream.h
@@ -2,24 +2,31 @@
 #define COBFS4_STREAM
 
 #include <stdint.h>
+#include <stdbool.h>
 #include <sys/socket.h>
 
 #include "constants.h"
 #include "siphash.h"
 #include "random.h"
 
-struct cobfs4_stream {
-    int fd;
-    struct siphash_ctx siphash;
-    struct rng_state rng;
-    uint8_t buffered_plaintext[COBFS4_MAX_DATA_LEN];
-    uint8_t buffered_request[COBFS4_MAX_FRAME_LEN];
+enum connection_type {
+    COBFS4_CLIENT,
+    COBFS4_SERVER,
 };
 
-int cobfs4_connect(struct cobfs4_stream *stream, const struct sockaddr *address, socklen_t address_len);
-int cobfs4_bind(struct cobfs4_stream *stream, const struct sockaddr *address, socklen_t address_len);
-int cobfs4_accept(struct cobfs4_stream *stream, const struct sockaddr *address, socklen_t address_len);
-int cobfs4_read(struct cobfs4_stream *stream, uint8_t buffer[static restrict COBFS4_MAX_DATA_LEN]);
-int cobfs4_write(struct cobfs4_stream *stream, uint8_t *buffer, size_t buff_len);
+struct cobfs4_stream {
+    int fd;
+    enum connection_type type;
+    struct siphash_ctx siphash;
+    struct rng_state rng;
+    uint8_t read_buffer[COBFS4_MAX_DATA_LEN];
+    uint8_t write_buffer[COBFS4_MAX_FRAME_LEN];
+    bool initialized;
+};
+
+int cobfs4_server_init(struct cobfs4_stream *stream, int socket);
+int cobfs4_client_init(struct cobfs4_stream *stream, int socket);
+int cobfs4_read(struct cobfs4_stream * restrict stream, uint8_t buffer[static restrict COBFS4_MAX_DATA_LEN]);
+int cobfs4_write(struct cobfs4_stream * restrict stream, uint8_t * restrict buffer, size_t buf_len);
 
 #endif /* COBFS4_STREAM */
